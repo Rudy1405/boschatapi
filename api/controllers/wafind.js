717bc7b9a753d270edbe1461e5330ff0e4ed0c10
Rudy221 <rudy1405@hotmail.com>
2019-04-25 07:58:39 -0500

    2.0

--- api/controllers/wafind.js
+++ api/controllers/wafind.js
@@ -1,6 +1,7 @@
 const User = require("../models/user")
 const sendJSONresponse = require('./shared').sendJSONresponse
 const request = require('request')
+const spawn = require("child_process").spawn;
 
 let partnerId = "beta_bosch"
 let partnerKey = "4700fc1c26dd4e54ab26a0bc1c9dd40d"
@@ -10,6 +11,104 @@ let pass = "hack2019"
 let email = "hack2019+13@partstech.com"
 let shopname = "Hackathon Team 13"
 let key = "93c78077d4174a0abec9c6c900c65e0b"
+
+let accesToken = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJiZXRhLnBhcnRzdGVjaC5jb20iLCJleHAiOjE1NTYzNDYzOTcsInBhcnRuZXIiOiJ0ZXN0X3BhcnRuZXIiLCJ1c2VyIjoiZGVtb19oZWxlbiJ9.TvKSsMSGwgw32bdHnZCrR3p4yl-7TZVnhX8D3J3YBhY'
+
+//Data from vehicle to speed up request
+let yearID = 2010;
+let make = "Kia";
+let makeID
+let model = "Rio";
+let modelID = 0;
+let submodelID = 0;
+let engineID = 0;
+let engineVinID = 0;
+let engineDesignationID = 0;
+let fuelTypeID = 0;
+let cylinderHeadTypeId = 0;
+
+function getVehicleData(req, res) {
+    request({
+        url: 'https://api.beta.partstech.com/taxonomy/vehicles/makes?' + yearID + '=2017&model=',
+        method: 'GET',
+        auth: {
+            bearer: accesToken
+        }
+    }, function(error, response, body) {
+        if (error) {
+            console.log('Error sending messages: ', error)
+        } else if (response.body.error) {
+            console.log('Error: ', response.body.error)
+        } else {
+            if (response.body) {
+                let aux = response.body
+                for (const iterator of aux) {
+                    if (iterator.makeName == req.body.brand) {
+                        makeID = iterator.makeId
+                    }
+                }
+                request({
+                    url: 'https://api.beta.partstech.com//taxonomy/vehicles/models?year=' + yearID + '&make=' + makeID + '&submodel',
+                    method: 'GET',
+                    auth: {
+                        bearer: accesToken
+                    }
+                }, function(error, response, body) {
+                    if (error) {
+                        console.log('Error sending messages: ', error)
+                    } else if (response.body.error) {
+                        console.log('Error: ', response.body.error)
+                    } else {
+                        if (response.body) {
+                            let aux2 = response.body
+                            for (const iterator of aux2) {
+                                if (iterator.modelName == req.body.model) {
+                                    modelID = modelId
+                                }
+                            }
+                            request({
+                                url: 'https://api.beta.partstech.com/taxonomy/vehicles/submodels?year=' + yearID + '&make=' + makeID + '&model=' + modelID + '&engine=',
+                                method: 'GET',
+                                auth: {
+                                    bearer: accesToken
+                                }
+                            }, function(error, response, body) {
+                                if (error) {
+                                    console.log('Error sending messages: ', error)
+                                } else if (response.body.error) {
+                                    console.log('Error: ', response.body.error)
+                                } else {
+                                    submodelID = response.body.submodelID
+                                    request({
+                                        url: 'https://api.beta.partstech.com/taxonomy/vehicles/engines?year=' + yearID + '&make=' + makeID + '&model=' + modelID + '&submodel=' + submodelID + '',
+                                        method: 'GET',
+                                        auth: {
+                                            bearer: accesToken
+                                        }
+                                    }, function(error, response, body) {
+                                        if (error) {
+                                            console.log('Error sending messages: ', error)
+                                        } else if (response.body.error) {
+                                            console.log('Error: ', response.body.error)
+                                        } else {
+                                            engineID = response.body.engineId,
+                                                engineVinID = response.body.engineVinId,
+                                                engineDesignationID = response.body.engineDesignationId,
+                                                fuelTypeID = response.body.fuelTypeId,
+                                                cylinderHeadTypeId = response.body.cylinderHeadTypeId,
+                                                //get submodelID engineID engineVinID  engineDesignationID fuelTypeID  cylinderHeadTypeId by comparation
+                                                getItemFromText(req, res, engineID, engineVinID, engineDesignationID, fuelTypeID, cylinderHeadTypeId)
+                                        }
+                                    })
+                                }
+                            })
+                        }
+                    }
+                })
+            }
+        }
+    })
+}
 
 
 
@@ -47,25 +146,13 @@ async function getItemFromImage(req, res) {
         this data is going to be sended to validate data
     */
 
+    /// based on https://www.pyimagesearch.com/2018/04/16/keras-and-convolutional-neural-networks-cnns/
 
-    request({
-        url: ' URI',
-        qs: { access_token: key },
-        method: 'GET',
-        json: {
-            recipient: { image: req.image },
-        }
-    }, function(error, response, body) {
-        if (error) {
-            console.log('Error sending messages: ', error)
-        } else if (response.body.error) {
-            console.log('Error: ', response.body.error)
-        } else if (response.body.arr) {
-            validateItem(res, response.body.arr[0], response.body.arr[1], response.body.arr[2], response.body.arr[3])
-        }
-    })
-
-
+    const pythonProcess = spawn('python', ["./clasifier/script.py", req.body.photourl])
+    pythonProcess.stdout.on('data', (data) => {
+        // Do something with the data returned from python script
+        getItemFromText(req, res, data.engineID, data.engineVinID, data.engineDesignationID, data.fuelTypeID, data.cylinderHeadTypeId)
+    });
 
 }
 
@@ -93,34 +180,118 @@ async function validateItem(req, res, partType, year, brand, model) {
 
 }
 
+function autorizacion(req, res) {
+    let json = {
+        accessType: "partner",
+        credentials: {
+            partner: {
+                id: "test_partner",
+                key: "97687715735f4b7da49d39bc19d0e532"
+            }
+        }
+    }
+}
 
-async function getItemFromText(req, res) {
-    /// req.partType
-    /// req.carYear
-    /// req.brand
-    // req.model
 
-    /**
-     * This is an example, we must test how a request JSON looks on postman,
-     * it gonna be a series of functions that find on diferent request from another
-     * so we can have the final result or ID
-     */
+
+/*function getVehicleNameFromText(req,res){
+    request({
+        url: 'https://api.beta.partstech.com/catalog/parts/BBSC-18042',
+        method: 'GET',
+        auth:{
+            bearer: accesToken
+        }
+        
+    }, function (error, response, body) {
+        if (error) {
+            console.log('Error sending messages: ', error)
+        } else if (response.body.error) {
+            console.log('Error: ', response.body.error)
+        } else {
+            res.json(body)
+            //console.log(response)
+        }
+    })
+ 
+ 
+}*/
+
+function getItemFromText(req, res, engineID, engineVinID, engineDesignationID, fuelTypeID, cylinderHeadTypeId) {
 
     request({
-        url: 'BOSCH URI',
-        qs: { access_token: key },
+        url: 'https://api.beta.partstech.com/catalog/parts/BBSC-18042',
         method: 'POST',
+        auth: {
+            bearer: accesToken
+        },
         json: {
-            recipient: { id: userId },
-            message: messageData,
+            searchParams: {
+                vehicleParams: {
+                    yearId: yearID,
+                    makeId: makeID,
+                    modelId: modelID,
+                    subModelId: submodelID,
+                    engineId: engineID,
+                    engineParams: {
+                        engineVinId: engineVinID,
+                        engineDesignationId: engineDesignationID,
+                        fuelTypeId: fuelTypeID,
+                        cylinderHeadTypeId: cylinderHeadTypeId
+                    }
+                },
+                keyword: req.body.partType
+            }
         }
     }, function(error, response, body) {
         if (error) {
             console.log('Error sending messages: ', error)
         } else if (response.body.error) {
             console.log('Error: ', response.body.error)
+        } else {
+
+            let gallery = {
+                messages: [{
+                    attachment: {
+                        type: "template",
+                        payload: {
+                            template_type: "generic",
+                            image_aspect_ratio: "square",
+                            elements: [{
+                                    title: response.body[1].partName,
+                                    image_url: "https://img1.partstech.com/d2/images/9e/67/fd/w110_9e67fd3844c7e250eb5bf5de4c98d1857db676f0.png",
+                                    subtitle: response.body[1].partTypeName + " " + response.body[1].vehicleName,
+                                    buttons: [{
+                                        type: "web_url",
+                                        url: "https://bcs.beta.partstech.com/Bosch-Disc-Brake-Rotor/details/BBHK-50011256?part_term=1896",
+                                        title: "View Item"
+                                    }]
+                                },
+                                {
+                                    title: response.body[2].partName,
+                                    image_url: "https://img1.partstech.com/d2/images/9e/67/fd/w110_9e67fd3844c7e250eb5bf5de4c98d1857db676f0.png",
+                                    subtitle: response.body[2].partTypeName + " " + response.body[2].vehicleName,
+                                    default_action: {
+                                        type: "web_url",
+                                        url: "https://bcs.beta.partstech.com/Bosch-Disc-Brake-Rotor/details/BBHK-50011256?part_term=1896",
+                                        messenger_extensions: true
+                                    },
+                                    buttons: [{
+                                        type: "web_url",
+                                        url: "https://bcs.beta.partstech.com/Bosch-Disc-Brake-Rotor/details/BBHK-50011256?part_term=1896",
+                                        title: "View Item"
+                                    }]
+                                }
+                            ]
+                        }
+                    }
+                }]
+            }
+            res.json(body)
+                //console.log(response)
         }
     })
+
+
 }
 
 
@@ -148,7 +319,7 @@ function tst(req, res) {
 }
 
 module.exports = {
-
+    getVehicleData,
     createWaUser,
     getItemFromImage,
     getItemFromText,
